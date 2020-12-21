546fa5d86cefbf2d09860123cb66836dd09b1245
eswarclynn <esuclintblazer@gmail.com>
2020-12-21 22:15:57 +0530

    Custom Color Scheme.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,5 @@
-import '../style/index.css'
+import "../style/custom.scss";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
