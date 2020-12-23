2a9d0bad5c5d2a28e5cdcb54d4e8c05fd0b59aba
eswarclynn <esuclintblazer@gmail.com>
2020-12-23 15:27:39 +0530

    Remove context from index.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,10 @@
-import { Container } from "react-bootstrap";
 import CharacterList from "../components/CharacterList";
 import Layout from "../components/Layout";
-import SearchBar from "../components/SearchBar";
-import { CharactersProvider } from "../context/CharactersContext";
 
 export default function Home({ data }) {
   return (
     <Layout>
-      <CharactersProvider>
-        <CharacterList />
-      </CharactersProvider>
+      <CharacterList />
     </Layout>
   );
 }
